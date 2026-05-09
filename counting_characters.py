8bf8d10119feec850791814144f424610aecc14a
Cara McCrossan <cara.mccrossan@icloud.com>
2026-05-09 15:49:00 +0100

    refactorerd counting characters and added notes. Updates readme

--- counting_characters.py
+++ counting_characters.py
@@ -8,11 +8,23 @@ def count(s):
 
     return dict
 
+def count_refectored(s):
+    return {char: s.count(char) for char in set(s)}
 
+
+print(count_refectored('aba'))
 """
+Attpt 1
 Full loop used to check if a key was not already in a dictionary 
 Add it if its not, increment the value by 1 if it is
 
 Return the dictionary
-"""
-print(count('aba'))
+
+Attpt 2
+Used set(s)
+ - which removes any duplicates and only returns one instance 
+ - s.count(char) counts each instance of the char in the string s
+ - x: assigns that count
+ 
+ This is wrapped in a dictionary comprehension {key: value for ...}
+ """
