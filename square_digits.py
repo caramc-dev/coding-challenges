c80e295bc4699fc2402cc8c3c9d8b3bb99db2c49
Cara McCrossan <cara.mccrossan@icloud.com>
2026-05-09 14:49:35 +0100

    add c hallenge & updated readme

---
+++ square_digits.py
@@ -0,0 +1,28 @@
+def square_digits(nums):
+    return int("".join(str(int(num)**2) for num in str(nums)))
+
+print(square_digits(9119))
+
+
+"""
+Attpt 1
+converted number to string to allow iteration
+converted back to int when returning to do the calculation
+After converting to int, within the loop back to a string to allow to be joined
+wrap the whole thing in an int to revert back to the int data type. 
+
+"""
+
+
+"""
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+Note: The function accepts an integer and returns an integer.
+
+Happy Coding!
+
+"""
