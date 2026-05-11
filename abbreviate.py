0a6c7ab72557be1ec4bb0115dbaaf3871fb83362
Cara McCrossan <cara.mccrossan@icloud.com>
2026-05-11 13:50:43 +0100

    add abbreviate.py

--- abbreviate.py
+++ abbreviate.py
@@ -1,11 +1,15 @@
 def abbrev_name(name):
-    return (char[0] for char in name.split(" "))
+    return ".".join([char.upper()[0] for char in name.split(" ")])
 
 
-print(abbrev_name("Sam Harris"))
+print(abbrev_name("sam Harris"))
 
 
 """
+Attmpt 1
+list comprehension to return the index[0] position as upper() when splitting on a space. 
+join using '.' to meet the required format.  
+
 Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
 
 The output should be two capital letters with a dot separating them.
